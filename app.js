8c6aa95fb1c552908f1ff47ec895ba8bbbc17537
caioandrademota <cam.snf19@uea.edu.br>
2022-01-08 18:46:40 -0400

    integração com a API do ViaCep

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 //seleção de elementos
-var submitButton = document.querySelector('#app form button');
-var zipCodeField = document.querySelector('#app form input');
-var content = document.querySelector('#app main')
+var submitButton = document.querySelector("#app form button");
+var zipCodeField = document.querySelector("#app form input");
+var content = document.querySelector("#app main");
 
-submitButton.addEventListener('click', run)
+submitButton.addEventListener("click", run);
 //monitoramento do evento click do elemento button
 
 function run(event) {
-    event.preventDefault();//prevenção de comportamento default
+  event.preventDefault(); //prevenção de comportamento default
 
-    var zipCode = zipCodeField.value;
-    //tratamento de erro
-    zipCode = zipCode.replace(' ', '');
-    zipCode = zipCode.replace('.', '');
-    zipCode = zipCode.trim();
+  var zipCode = zipCodeField.value;
+  //tratamento de erro
+  zipCode = zipCode.replace(" ", "");
+  zipCode = zipCode.replace(".", "");
+  zipCode = zipCode.trim();
 
-    console.log(zipCodeField.value);
+  axios
+    .get("https://viacep.com.br/ws/" + zipCode + "/json/") //realização da requisição com o Axios
+    .then((response) => {
+      content.innerHTML = "";
+      //manipulando a resposta
+      createLine(response.data.cep);
+      createLine(response.data.logradouro + ", " + response.data.bairro);
+      createLine(response.data.localidade + "/" + response.data.uf);
+      console.log(response.data);
+    })
+    .catch((err) => {
+      //manipulando os erros
+      console.log(err);
+    });
+
+  console.log(zipCodeField.value);
+}
+
+function createLine(text) {
+  var line = document.createElement("p");
+  var text = document.createTextNode(text);
+
+  line.appendChild(text);
+  content.appendChild(line);
 }
